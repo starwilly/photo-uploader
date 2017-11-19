05a4ef2964cd5bbc0eaf9961487d020962ff72c1
starwilly <willystar@gmail.com>
2017-11-19 23:25:26 +0800

    Add photo model

--- photo_uploader/backend/models.py
+++ photo_uploader/backend/models.py
@@ -7,6 +7,6 @@ from django.db import models
 class Photo(models.Model):
     """ Photo model """
     title = models.CharField(max_length=100, blank=True)
-    file = models.ImageField()
+    file = models.ImageField(upload_to='uploads/')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
