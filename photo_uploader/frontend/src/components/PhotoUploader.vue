40408e9071dd415b32657cbe957d547fcb99f354
starwilly <willystar@gmail.com>
2017-11-19 23:44:00 +0800

    Add PhotoUploader component

---
+++ photo_uploader/frontend/src/components/PhotoUploader.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="center">
+  <vue-core-image-upload
+    class="btn btn-primary"
+    :crop="false"
+    @imageuploaded="imageuploaded"
+    :max-file-size="5242880"
+    input-of-file='file'
+    :url="uploadUrl" >
+  </vue-core-image-upload>
+</div>
+</template>
+
+<script>
+import VueCoreImageUpload from 'vue-core-image-upload'
+
+export default {
+  name: 'PhotoUploader',
+  components: {
+    'vue-core-image-upload': VueCoreImageUpload
+  },
+  data () {
+    return {
+      uploadUrl: 'http://localhost:8000/api/upload'
+    }
+  },
+  methods: {
+    imageuploaded (res) {
+      if (res.success) {
+        alert(res.success)
+      }
+    }
+  }
+}
+</script>
